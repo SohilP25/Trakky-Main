e6ed3140d5809dc001cb415b45cf8ef5c831f688
kevintamakuwala <kevintamakuwala16@gmail.com>
2023-05-27 18:38:11 +0530

    Created Hero Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./Components/Common/Header/Header";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
-  return <>
-    <Header/>
-  </>;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
