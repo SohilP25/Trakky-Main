1a575a99894bffad38f052793d1682a65c6ebf9d
kevintamakuwala <kevintamakuwala16@gmail.com>
2023-06-02 15:22:42 +0530

    Offers and footer in SpaList

--- frontend/src/Components/SpaNearMePage/SpaNearMeList.jsx
+++ frontend/src/Components/SpaNearMePage/SpaNearMeList.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Hero from "./Hero/Hero";
-
+import Footer from "./../Common/Footer/Footer";
+import Slider from "../Common/Slider/Slider";
+import { offers } from "../../data";
 const SpaNearMeList = () => {
   return (
     <div>
       <Hero />
+
+      {/* Offer Starts------------------- */}
+      <div
+        className="slider__outer-container offer__container"
+        style={{ marginTop: "5rem", width: "90%" }}
+      >
+        <div className="slider__header">
+          <h2>Grab the best deals</h2>
+        </div>
+        <Slider cardList={offers} _name={"offer"} />
+      </div>
+      {/* Offer Ends */}
+
+      <Footer />
     </div>
   );
 };
