a3a3c38c75678a27a6ceba9edc7d0df24deb5333
sahilsapariya <sahilsapariya03@gmail.com>
2023-06-03 18:01:01 +0530

    created card list in spa list page

---
+++ frontend/src/Components/SpaPage/SpaCard/SpaCard.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+import './SpaCard.css'
+import { Link } from 'react-router-dom'
+import SpaImage from '../../../Assets/images/spa/spa.png'
+import CardIcons from '../../../Assets/images/icons/card_icons.png'
+import LikeIcon from '../../../Assets/images/icons/like.svg'
+import Man from '../../../Assets/images/icons/man.svg'
+import Star from '../../../Assets/images/icons/star.svg'
+
+
+const SpaCard = (props) => {
+  return (
+    <div className='spa_card'>
+      <div className="image__container">
+        <img src={SpaImage} alt="" />
+      </div>
+
+      <div className="spa_information__container">
+        <div className="spa_name">
+          <h2>Aroma the luxurious spa</h2>
+          <p>Kudasan, Gandhinagar</p>
+        </div>
+
+        <div className="spa_icon__box">
+          <img src={CardIcons} alt="" />
+        </div>
+
+        <div className="spa_rating__box">
+          <span>5.0</span>&nbsp;<img src={Star} style={{ width: '1.4rem' }} alt="" />&nbsp;(<span id='spa-review'>318</span>&nbsp;reviews)
+        </div>
+      </div>
+
+      <div className="spa_contact__container">
+        <div className="spa_offer_tag__box">
+          <p>couple therapy 50% off</p>
+          <img src={LikeIcon} alt="" />
+        </div>
+
+        <div className="spa_contact__box">
+          <div className="spa_price_tag">
+            <img src={Man} alt="" />
+            <p>&nbsp;₹ <span id='spa-price'>999</span> Onwards</p>
+          </div>
+
+          <div className="spa_booking_buttons">
+            <Link>Book Now</Link>
+            <Link>Call Now</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const SpaCardMini = () => {
+  return (
+    <div className="mini_spa_card">
+      <div className="main_mini_spa__container">
+        <div className="mini_image__container">
+          <img src={SpaImage} alt="" />
+          <p>couple therapy 50% off</p>
+        </div>
+
+        <div className="mini_spa_information__container">
+          <div className="spa_name">
+            <h2>Aroma the luxurious spa</h2>
+            <p>Kudasan, Gandhinagar</p>
+          </div>
+
+          <div className="spa_icon__box">
+            <img src={CardIcons} alt="" />
+          </div>
+
+          <div className="spa_rating__box">
+            <span>5.0</span>&nbsp;<img src={Star} alt="" />&nbsp;(<span id='spa-review'>318</span>&nbsp;reviews)
+          </div>
+
+          <div className="mini_spa_price__box">
+            <div className="mini_spa_price_tag">
+              <img src={Man} alt="" />
+              <p>&nbsp;₹ <span id='spa-price'>999</span> Onwards</p>
+            </div>
+
+            <div className="spa_like__box">
+              <img src={LikeIcon} alt="" />
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="mini_spa_buttons">
+        <div>
+          <Link>Book Now</Link>
+        </div>
+        <span style={{ alignContent: "center", display: "flex" }}>|</span>
+        <div>
+          <Link>Call Now</Link>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default SpaCard
--- frontend/src/Components/SpaPage/SpaList/SpaList.jsx
+++ frontend/src/Components/SpaPage/SpaList/SpaList.jsx
@@ -1,36 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './SpaList.css'
 
 import Hero from "../Hero/Hero";
 import Footer from "../../Common/Footer/Footer";
 import Slider from "../../Common/Slider/Slider";
 
 import { offers } from "../../../data";
+import SpaCard from "../SpaCard/SpaCard";
+import { SpaCardMini } from "../SpaCard/SpaCard";
 
 const SpaNearMeList = (props) => {
 
-
+  function getWindowDimensions() {
+    const width = window.innerWidth,
+      height = window.innerHeight;
+    return { width, height };
+  }
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div>
       <Hero />
-
-       
-
-      {/* Offer Starts------------------- */}
-      <div
-        className="slider__outer-container offer__container"
-        style={{ marginTop: "5rem", width: "90%" }}
-      >
-        <div className="slider__header">
-          <h2>Grab the best deals</h2>
-        </div>
-        <Slider cardList={offers} _name={"offer"} />
+      <div className="spa_list__header">
+        <h1>Spa Near Me</h1>
       </div>
-      {/* Offer Ends */}
+      {{ windowDimensions }.windowDimensions.width >= 765 ? (
+        <>
+          <SpaCard />
+          <SpaCard />
+
+          {/* Offer Starts------------------- */}
+          <div
+            className="slider__outer-container offer__container"
+            style={{ width: "90%" }}
+          >
+            <div className="slider__header">
+              <h2>Grab the best deals</h2>
+            </div>
+            <Slider cardList={offers} _name={"offer"} />
+          </div>
+          {/* Offer Ends */}
+          <SpaCard />
+          <SpaCard />
+          <SpaCard />
+          <SpaCard />
+
+        </>
+      ) : (
+        <>
+          <SpaCardMini />
+          <SpaCardMini />
+
+          {/* Offer Starts------------------- */}
+          <div
+            className="slider__outer-container offer__container"
+            style={{ width: "90%" }}
+          >
+            <div className="slider__header">
+              <h2>Grab the best deals</h2>
+            </div>
+            <Slider cardList={offers} _name={"offer"} />
+          </div>
+          {/* Offer Ends */}
+          <SpaCardMini />
+          <SpaCardMini />
+          <SpaCardMini />
+          <SpaCardMini />
+        </>
+      )}
+      <div className="view_more__button">
+        <button>View More</button>
+      </div>
 
       <Footer />
     </div>
   );
 };
+
 
 export default SpaNearMeList;
