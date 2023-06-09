0d77125de04cd8e099216fbb801b80f3d44c24ab
sahilsapariya <sahilsapariya03@gmail.com>
2023-06-09 21:54:31 +0530

    created many features on admin panel

--- Admin Frontend/src/pages/Dashboard.jsx
+++ Admin Frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { SpaData } from '../data/mockData.js'
+import './Dashboard.css'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='dashboard__container'>
+      <div className="spa_data_labels">
+        <div className="data__container">
+          <p>No of Spa</p>
+          <h1>{SpaData.length}</h1>
+        </div>
+
+        <div className="data__container">
+          <p>No of Daily Logs</p>
+          <h1>10</h1>
+        </div>
+
+        <div className="data__container">
+          <p>No of Weekly Logs</p>
+          <h1>70</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
