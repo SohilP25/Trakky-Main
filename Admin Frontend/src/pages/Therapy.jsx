234b07a60c954c7d230681b2d14acfa18da3eb47
kevintamakuwala <kevintamakuwala16@gmail.com>
2023-06-09 17:09:56 +0530

    New Admin Frontend

---
+++ Admin Frontend/src/pages/Therapy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Page.css';
+
+const Therapy = () => {
+
+
+  return (
+    <div className="therapy__container">
+      <h1>Therapy</h1>
+    </div>
+  )
+}
+
+export default Therapy
+
+
+
+
