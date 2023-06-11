8cf10aaf858e5624bc61ee73cee01bdb90beebe3
patelvishal93137 <patelvishal93137@gmail.com>
2023-06-11 10:03:10 +0530

    offer creation

--- Backend/Models/offerModel.js
+++ Backend/Models/offerModel.js
@@ -8,14 +8,31 @@ const offerScheme = new mongoose.Schema({
         type : String,
         required : true,
     },
+    Slug : {
+        type : String,
+        required : true,
+    },
+    Select_Spa : {
+        type : String,
+        required : true,
+    },
+    Priority : {
+        type : Number,
+        required : true,
+        validate: {
+            validator: async function (priority) {
+              // Check if any other therapy already has this priority assigned
+              const count = await mongoose.models.OfferModel.countDocuments({ Priority: priority });
+              return count === 0; // Return true if no other therapy has this priority assigned
+            },
+            message: "Priority already assigned to another therapy.",
+          },
+          
+    },
     Image : {
         data : Buffer,
         contentType : String,
     },
-    Discount : {
-        type : String,
-        required : true,
-    }
 }   
 );
 
