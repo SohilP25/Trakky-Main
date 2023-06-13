20157227a539291c95582846ef65a4842fe2479a
kevintamakuwala <kevintamakuwala16@gmail.com>
2023-06-13 15:43:25 +0530

    Offer Post Request

--- Backend/Middleware/ImageMiddleware.js
+++ Backend/Middleware/ImageMiddleware.js
@@ -1,23 +1,23 @@
 import fs, { mkdir } from "fs";
 import path from "path";
-import multer from 'multer';
+import multer from "multer";
 const storage = multer.diskStorage({
-    function (req,res,cb) {
-        if(!fs.existsSync('public')){
-                fs.mkdirSync('public');
-        }
-        if(!fs.existsSync('public/files')){
-            fs.mkdirSync('public/files');
-        }
-        cb(null,'public/files');
-    },
-    filename : function(req,file,cb){
-            console.log(file)
-            cb(null,Date.now() + file.originalname);
+  function(req, res, cb) {
+    if (!fs.existsSync("public")) {
+      fs.mkdirSync("public");
     }
-})
+    if (!fs.existsSync("public/files")) {
+      fs.mkdirSync("public/files");
+    }
+    cb(null, "public/files");
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
 const upload = multer({
-    storage : storage,
-})
+  storage: storage,
+});
 export default upload;
